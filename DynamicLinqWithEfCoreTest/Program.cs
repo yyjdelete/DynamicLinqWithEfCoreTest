2bff2b711c90933ed1c7208246175e85e6cd9ecd
SilverFox <yyjdelete@126.com>
2018-05-11 23:21:46 +0800

    Test for StefH/System.Linq.Dynamic.Core#165

--- DynamicLinqWithEfCoreTest/Program.cs
+++ DynamicLinqWithEfCoreTest/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
 
 namespace DynamicLinqWithEfCoreTest
 {
@@ -31,38 +34,98 @@ namespace DynamicLinqWithEfCoreTest
 
             using (var context = new TestContext())
             {
-                context.Set<Person>().AddRange(list);
-                context.SaveChanges();
-            }
-
-            const int num = 100_000;
-
-            var sw = Stopwatch.StartNew();
-
-            for (var i = 0; i < num; i++)
-            {
-                using (var context = new TestContext())
+                try
                 {
-                    var set = context.Set<Person>().Where(m => m.Age == 1 && m.Name == "tom").ToList();
+                    //DynamicExpressionParser.ParseLambda(new ParsingConfig { UseDynamicObjectClassForAnonymousTypes = true }, null, "new {1 AS Id}");
+
+                    context.Database.EnsureCreated();
+
+                    context.Set<Person>().AddRange(list);
+                    context.SaveChanges();
+
+                    context.Set<Person>().Select(d => new Tuple<int>(d.Age)).GroupBy(d => new Tuple<int>(d.Item1)).Select("new {Key.Item1, Sum(Item1) As Value}").TryLogToList("exp: Tuple,Tuple");
+                    context.Set<Person>().Select(d => new { Id = d.Age }).GroupBy(d => new { Id = d.Id }).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("exp: Anonymous,Anonymous");
+                    context.Set<Person>().Select(d => new { Id = d.Age }).GroupBy(d => new Tmp1 { Id = d.Id }).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("exp: Anonymous,Tmp1; should failed");
+                    context.Set<Person>().Select(d => new { Id = d.Age }).GroupBy(d => new Tmp2 { Id = d.Id }).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("exp: Anonymous,Tmp2; should failed");
+                    context.Set<Person>().Select(d => new Tmp1 { Id = d.Age }).GroupBy(d => new { Id = d.Id }).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("exp: Tmp1,Anonymous; should failed");
+                    context.Set<Person>().Select(d => new Tmp2 { Id = d.Age }).GroupBy(d => new { Id = d.Id }).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("exp: Tmp2,Anonymous; should failed");
+
+                    context.Set<Person>().Select("new { Age AS Id }").GroupBy("new { Id }").Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("dyn: Anonymous,Anonymous");
+                    //Note the changed in #117 for Anonymous Type is also needed
+                    context.Set<Person>().Select4Test("new { Age AS Id }", true).GroupBy4Test("new { Id }", true).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("dyn: Anonymous,Anonymous; with createParameterCtor=true");
+                    context.Set<Person>().Select4Test("new { Age AS Id }", false).GroupBy4Test("new { Id }", false).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("dyn: Anonymous,Anonymous; with createParameterCtor=false");
+
+                    context.Set<Person>().Select<Tmp1>("new { Age AS Id }").GroupBy(DynamicExpressionParser.ParseLambda<Tmp1, Tmp1>(null, false, "new { Id }")).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("dyn: Tmp1,Tmp1(without ParameterCtor)");
+                    context.Set<Person>().Select<Tmp2>("new { Age AS Id }").GroupBy(DynamicExpressionParser.ParseLambda<Tmp2, Tmp2>(null, false, "new { Id }")).Select("new {Key.Id, Sum(Id) As Value}").TryLogToList("dyn: Tmp2,Tmp2(with ParameterCtor)");
+                }
+                finally
+                {
+                    context.Database.EnsureDeleted();
                 }
             }
 
-            Console.WriteLine(sw.Elapsed);
-
-            sw.Restart();
-
-            for (var i = 0; i < num; i++)
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+        //one class without createParameterCtor 
+        public class Tmp1
+        {
+            public Tmp1()
             {
-                using (var context = new TestContext())
-                {
-                    var set = context.Set<Person>().Where("$.Age==@0 and $.Name==@1", 1, "tom").ToList();
-                }
             }
+            public int Id { get; set; }
+        }
+        //one class with createParameterCtor 
+        public class Tmp2
+        {
+            public Tmp2()
+            {
+            }
+            public Tmp2(int id)
+            {
+            }
+            public int Id { get; set; }
+        }
+    }
 
-            Console.WriteLine(sw.Elapsed);
+    internal static class TestUtils
+    {
+        public static IQueryable Select4Test(this IQueryable source, string selector, bool createParameterCtor)
+        {
+            LambdaExpression lambda = DynamicExpressionParser.ParseLambda(null, createParameterCtor, source.ElementType, null, selector);
 
-            //Console.WriteLine("Press any key to quit");
-            //Console.ReadKey();
+            var optimized = Expression.Call(
+                typeof(Queryable), nameof(Queryable.Select),
+                new[] { source.ElementType, lambda.Body.Type },
+                source.Expression, Expression.Quote(lambda));
+
+            return source.Provider.CreateQuery(optimized);
+        }
+
+        public static IQueryable GroupBy4Test(this IQueryable source, string keySelector, bool createParameterCtor)
+        {
+            var keyLambda = DynamicExpressionParser.ParseLambda(null, createParameterCtor, source.ElementType, null, keySelector);
+
+            var optimized = Expression.Call(
+                typeof(Queryable), nameof(Queryable.GroupBy),
+                new[] { source.ElementType, keyLambda.Body.Type }, source.Expression, Expression.Quote(keyLambda));
+
+            return source.Provider.CreateQuery(optimized);
+        }
+
+        public static List<dynamic> TryLogToList(this IQueryable queryable, string tag)
+        {
+            try
+            {
+                var res = queryable.ToDynamicList();
+                Console.Error.WriteLine($"Success query with {tag}." + Environment.NewLine);
+                return res;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Failed query with {tag}:" + Environment.NewLine + e.Message + Environment.NewLine);
+                return null;
+            }
         }
     }
 
@@ -88,7 +151,8 @@ namespace DynamicLinqWithEfCoreTest
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseInMemoryDatabase("test");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test165;Trusted_Connection=True;")
+                .ConfigureWarnings(warns=>warns.Throw(RelationalEventId.QueryClientEvaluationWarning));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
